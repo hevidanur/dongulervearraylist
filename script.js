f4dcbdfbab3c037047d5ae20e75dfcc1674fa9be
hevidanur <hevidanuruludag@gmail.com>
2024-09-26 18:08:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,86 @@
+alert("selammm")
+/*var sayi=20;
+var sayi2=30;
+var sayi3=40;
+alert("toplamları:"+(sayi+sayi2+sayi3));
+3 sayıyı(sayi,sayi2,sayi3) toplama*/
+
+
+/*var a="javascript";
+var b=" programlama";
+var c=" dili";
+alert(a+b+c);
+
+stringleri toplayıp (java script programlama dili)alert ile mesaj verdirebilme
+*/
+
+/*var a;
+a=10;
+var b= a*2+10;
+alert("b'nin değeri"+b);
+
+a yı 2 ile çarpıp 10 ile toplayan program(sonuç=30)*/
+
+
+/* ARRAYLİST
+
+var array =["java","python","c++","c"]
+alert(array[0]);
+
+,burada 0.indexte ki yani "java" yazısını ekrana verir o 
+köşeli parantezin içersine 0 yerine 3 yazılsaydı eğer "c"
+yazısı verilirdi*/
+
+
+
+/*var array =["java","python","c++","c"]
+alert(array[0]);
+alert(array[1]);
+alert(array[2]);
+alert(array[3]);
+
+array.push("go");   //burada arraylist e bir elemean ekledik
+array[4]="php";     //burada da array list e bir eleman ekledik 
+                      push metodu ile arasın da ki fark istediğimiz index e
+                      ekleyebiliyoruz
+
+array(array.length);   //arrayimiz de kaç tane eleman oldudğunu
+                         gösterir
+
+burada ki kodun çıktısı =java,phyton,c++,c olacaktır ama 
+sıra sıra tek tek vericek hepsini bir arada değil
+
+*/
+
+/*FOR DÖNGÜSÜ
+
+for (var i=0;i<=10;i++){
+console.log("i değeri "+i);
+}
+
+F12 ye tıklandığı zaman console kısmında çıkıcak.
+çıkıcak değer=0,1,2,3,4,5,6,7,8,9,10
+*/
+
+/*WHILE DÖNGÜSÜ
+var i=0;
+while(i<100){
+console.log("i degeri"+i);
+i+=2;
+
+}
+ burada while döngüsü ile  çift sayıları gösterir(F12 console kısmından)
+*/
+
+/*
+var sayilar=[32,5,1,100,-1,90]
+alert(sayilar.length);
+
+console.log("array elemanları...");
+for(i=0;i<sayilar.length;i++){
+    console.log("eleman"+ sayilar[i]);
+}
+
+liste de ki eleman sayılarının for döngüsü ile eleman sayılarını bulmak
+
+*/
